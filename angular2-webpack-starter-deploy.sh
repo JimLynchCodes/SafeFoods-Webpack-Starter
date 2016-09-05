f18ae92f74730b0810d42a81810ba423dba7d702
JimTheMan <mrdotjim@gmail.com>
2016-09-05 13:14:47 -0400

    deploy

---
+++ angular2-webpack-starter-deploy.sh
@@ -0,0 +1,44 @@
+echo "Target: gh-pages branch"
+  
+ORIGIN_URL=`git config --get remote.origin.url`  
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+
+git config --global user.name "JimTheMan" || exit 1  
+git config --global user.email "mrdotjim@gmail.com" || exit 1
+
+echo "Cleaning up temp files"  
+# rm -Rf $TEMP_DIRECTORY
+
+echo "Deploying..."
+ 
+git push origin :gh-pages
+git commit -am 'commit from paris, france.'
+git subtree push --prefix angular2-webpack-starter/dist/ origin gh-pages || {
+echo "something bad happened"
+echo "deleting gh-pages"
+git branch -D gh-pages
+echo "checking out orphan"
+git checkout --orphan gh-pages
+echo "adding"
+git add --all
+echo "committing"
+git commit -m 'gh-pages commit!!'
+echo "pushing gh-pages"
+git push origin gh-pages
+
+echo "checking out master"
+git checkout master
+echo "entering"
+cd angular2-webpack-starter
+echo "building dist folder"
+npm run build:prod
+echo "deleting old gh-pages"
+git push origin :gh-pages
+echo "pushing dist!"
+cd ..
+git subtree push --prefix angular2-webpack-starter/dist/ origin gh-pages
+
+
+}
+
+echo "Deployed."  
